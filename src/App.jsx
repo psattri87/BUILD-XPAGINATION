6beaff0a91b53123b04ccfad4a9dee9741bd9910
psattri87 <psattri87@gmail.com>
2025-01-05 12:28:17 +0530

    build pagination app

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageData, setPageData] = useState([]);
+  const totalPage = Math.ceil(data.length / 10);
+
+  function handlePrevious() {
+    setCurrentPage(currentPage - 1);
+  }
+
+  function handleNext() {
+    setCurrentPage(currentPage + 1);
+  }
+
+  useEffect(() => {
+    const startIndex = (currentPage - 1) * 10;
+    let endIndex = startIndex + 9;
+    if (endIndex > data.length - 1) {
+      endIndex = data.length - 1;
+    }
+    const array = new Array();
+    for (let i = startIndex; i <= endIndex; i++) {
+      array.push(data[i]);
+    }
+    setPageData(array);
+  }, [currentPage, data]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(
+          "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+        );
+        if (response.status === 200) {
+          setData(response.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Employee Data Table</h1>
+
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pageData.length > 0 &&
+            pageData.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.role}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+      <div className="buttons">
+        <button onClick={handlePrevious} disabled={currentPage === 1}>
+          Previous
+        </button>
+        <div className="cp">{currentPage}</div>
+        <button onClick={handleNext} disabled={currentPage === totalPage}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
